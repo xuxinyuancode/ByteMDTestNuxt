c41a2d3a2524424f5a983268089a0868e2abd5e9
xuxinyuan <204663476@qq.com>
2022-05-15 21:33:29 +0800

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,39 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="
+      relative
+      flex
+      items-top
+      justify-center
+      min-h-screen
+      bg-gray-100
+      sm:items-center sm:pt-0
+    "
+  >
+    <Editor :value="value" :plugins="plugins" @change="handleChange" />
+  </div>
+</template>
+
+<script>
+import "bytemd/dist/index.css";
+import { Editor, Viewer } from "@bytemd/vue";
+import mermaid from "@bytemd/plugin-mermaid";
+const plugins = [
+  mermaid(),
+  // Add more plugins here
+];
+
+export default {
+  components: { Editor },
+  name: "NuxtTutorial",
+  data() {
+    return { value: "", plugins };
+  },
+  methods: {
+    handleChange(v) {
+      this.value = v;
+    },
+  },
+};
+</script>
